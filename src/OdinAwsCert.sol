1a3251695b0468784b00e2d132483366ef521fed
Odin Thif <mjd.thif@icloud.com>
2025-08-02 02:25:23 +0200

    #2  nft contract set up | added natspec for the the contracts functions

--- src/OdinAwsCert.sol
+++ src/OdinAwsCert.sol
@@ -10,6 +10,16 @@ contract AwsCert is ERC721 {
     constructor() ERC721("CERTI", "ODINAWSCERT") {
         s_tokenIdCounter = 0;
     }
+    /**
+     * @dev Mints a new NFT with the given token URI.
+     * This function allows anyone to mint a new NFT by providing a token URI.
+     * The token URI is stored in a mapping for later retrieval.
+     * The token ID is automatically incremented for each new mint.
+     * @notice This function is public and can be called by anyone.
+     * @dev The function uses the `_safeMint` method from the ERC721 contract to
+     * ensure that the minting process is safe and compliant with the ERC721 standard.
+     * @param newTokenURI The URI for the new token.
+     */
 
     function mintCertNft(string memory newTokenURI) public {
         // take the uri and store it in the mapping | we stored the tokenURI in the mapping
@@ -20,6 +30,11 @@ contract AwsCert is ERC721 {
         s_tokenIdCounter++;
     }
 
+    /**
+     * @dev Returns the token URI for a given token ID.
+     * @param tokenId The ID of the token to query.
+     * @return The URI of the token.
+     */
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         if (ownerOf(tokenId) == address(0)) {
             revert("Token does not exist");
