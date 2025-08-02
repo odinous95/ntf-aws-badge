f85e2b649dd98868946b049d7ec18e588bc30d61
Odin Thif <mjd.thif@icloud.com>
2025-08-02 02:36:24 +0200

    changed name of the nft contract to CertNFT

--- src/OdinAwsCert.sol
+++ src/OdinAwsCert.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract AwsCert is ERC721 {
+contract CertNFT is ERC721 {
     uint256 private s_tokenIdCounter;
     mapping(uint256 tokenId => string tokenUri) private tokenIdToUri;
 
