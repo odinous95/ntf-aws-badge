175ca7e06b22a5239dac048400d220209e4ee2dc
Odin Thif <mjd.thif@icloud.com>
2025-08-02 02:36:35 +0200

    #1  deployment set up

---
+++ script/DepolyContract.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {CertNFT} from "../src/OdinAwsCert.sol";
+
+contract deployContrct is Script {
+    function run() external returns (CertNFT) {
+        vm.startBroadcast();
+        CertNFT certNFT = new CertNFT();
+        vm.stopBroadcast();
+        return certNFT;
+    }
+}
