b3785a919ccae999c5ce534b4fc5d719a334957b
Odin Thif <mjd.thif@icloud.com>
2025-08-04 17:01:24 +0200

    corrected name

--- script/DepolyContract.s.sol
+++ script/DepolyContract.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {CertNFT} from "../src/OdinAwsCert.sol";
 
-contract deployContrct is Script {
+contract DeployContract is Script {
     function run() external returns (CertNFT) {
         vm.startBroadcast();
         CertNFT certNFT = new CertNFT();
